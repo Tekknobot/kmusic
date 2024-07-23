260bd4d1cf725ea13c0141a5650ac926a19c207f
Tekknobot <thrillatron@hotmail.com>
2024-07-23 01:13:14 -0400

    add pause toggle

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AudioHelm;
+
+public class Pause : MonoBehaviour
+{
+    // Reference to the Audio Helm Clock
+    public AudioHelmClock helmClock;
+
+    // Reference to the Toggle UI component
+    public Toggle pauseToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (helmClock != null && pauseToggle != null)
+        {
+            // Set the clock to unpause (playing) on start
+            helmClock.pause = false;
+
+            // Initialize the Toggle's state based on the clock's paused state
+            pauseToggle.isOn = !helmClock.pause;
+
+            // Add listener for toggle value changes
+            pauseToggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+    }
+
+    // Called when the Toggle value changes
+    private void OnToggleValueChanged(bool isOn)
+    {
+        if (helmClock != null)
+        {
+            // Reverse the logic for setting pause state
+            helmClock.pause = !isOn;
+        }
+    }
+}
