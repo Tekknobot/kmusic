f63c05f3958176d6220dbd0bdb1db786f8b300e0
Tekknobot <thrillatron@hotmail.com>
2024-07-24 11:51:48 -0400

    push from mac

--- Assets/Scripts/KitButton.cs
+++ Assets/Scripts/KitButton.cs
@@ -6,11 +6,22 @@ using AudioHelm;
 public class KitButton : MonoBehaviour
 {
     public AudioHelm.Sampler sampler;
+    public DrumSamples drumSamples;
 
     // Start is called before the first frame update
     void Start()
     {
         sampler = GameObject.Find("Sequencer").GetComponent<Sampler>();
+        drumSamples = drumSamples.GetComponent<DrumSamples>(); // Get the DrumSamples component
+    }
+
+    // Method to handle button click event
+    public void OnButtonClick()
+    {
+        if (gameObject.name == "707")
+        {
+            //SwapSamplesWithRockArray();
+        }
     }
 
     // Update is called once per frame
